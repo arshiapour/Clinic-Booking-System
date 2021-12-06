2d81cf6c26b6455465317729641616f6c6c28cd5
Arshia <arshiapour@hotmail.co.uk>
2021-12-06 21:43:15 +0000

    Appointment database added and some extra configurations

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,7 @@
 class PatientsController < ApplicationController
   before_action :set_patient, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+  #before_action :correct_user, only: [:edit, :update, :destroy]
 
   # GET /patients or /patients.json
   def index
@@ -13,6 +15,7 @@ class PatientsController < ApplicationController
   # GET /patients/new
   def new
     @patient = Patient.new
+    #patient = current_user.patient.build
   end
 
   # GET /patients/1/edit
@@ -22,6 +25,7 @@ class PatientsController < ApplicationController
   # POST /patients or /patients.json
   def create
     @patient = Patient.new(patient_params)
+    #@patient = current_user.patient.build(patient_params)
 
     respond_to do |format|
       if @patient.save
@@ -56,6 +60,11 @@ class PatientsController < ApplicationController
     end
   end
 
+  def correct_user
+    @patient = current_user.patient.find_by(id: params[:id])
+    redirect_to patients_path, notice:"Access Denied" if @patient.nil?
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_patient
@@ -64,6 +73,6 @@ class PatientsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def patient_params
-      params.require(:patient).permit(:first_name, :last_name, :email, :phone)
+      params.require(:patient).permit(:first_name, :last_name, :email, :phone, :appointment_id, :user_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_021900) do
+ActiveRecord::Schema.define(version: 2021_12_06_201552) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string "first_name"
@@ -19,6 +27,9 @@ ActiveRecord::Schema.define(version: 2021_12_03_021900) do
     t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "appointment_id"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_patients_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
